c319073f247d8b8042ed9dca3bfbfc37572d33fc
tebarek <tebarek29@gmail.com>
2025-02-26 11:33:32 +0300

    added .env

--- public/Logo.tsx
+++ public/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export function Logo() {
     return (
       <div className="flex items-center">
-        <Image src="/logo.svg" alt="YourLogo" width={32} height={32} className="mr-2" />
+        <Image src="/logo.svg" alt="I Property Solution" width={32} height={32} className="mr-2" />
         <span className="text-lg font-bold">I Property Solution</span>
       </div>
     )
